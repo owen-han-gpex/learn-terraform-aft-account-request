88e24ea380e2e6c7e4222c974206d578757be587
owen <owen@kpxdx.io>
2024-03-11 13:49:53 +0900

    Add sandbox account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "owen+tc2_sandbox@kpxdx.com"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "sandbox"
+    SSOUserEmail              = "owen+tc2_sandbox@kpxdx.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
